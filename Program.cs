39181cef533ccec0b1d407746ae1c394e507fc31
Payton Hansen <paymeaton12@gmail.com>
2025-09-09 14:47:25 -0600

    skeleton of class made

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System.Data.Common;
+
+class Task
+{
+    private int id;
+    private string title;
+    private string description;
+    private bool isComplete;
+
+    public Task()
+    {
+        
+    }
+    public int ID { get; set; }
+    public string Title{get; set;}
+    public string Description{get; set;}
+    public bool IsComplete{get; set;}
+
+    public void DisplayTask()
+    {
+
+    }
+
+    public void DislayDescription()
+    {
+
+    }
+
+    public void MarkAsCompleted()
+    {
+
+    }
+}
